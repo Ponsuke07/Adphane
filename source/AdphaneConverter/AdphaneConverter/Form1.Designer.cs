a0304aaf8ce476397ff416b66427a838053356bd
大典 山根 <s15716@yonago.kosen-ac.jp>
2016-09-22 18:02:42 +0900

    Add project

---
+++ source/AdphaneConverter/AdphaneConverter/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Cvsharp_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.file_select = new System.Windows.Forms.Button();
+            this.Convert_buttom = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.gamma_bar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamma_bar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(15, 22);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(200, 300);
+            this.pictureBoxIpl1.TabIndex = 0;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // file_select
+            // 
+            this.file_select.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.file_select.Location = new System.Drawing.Point(15, 362);
+            this.file_select.Name = "file_select";
+            this.file_select.Size = new System.Drawing.Size(98, 34);
+            this.file_select.TabIndex = 1;
+            this.file_select.Text = "ファイルの選択";
+            this.file_select.UseVisualStyleBackColor = true;
+            this.file_select.Click += new System.EventHandler(this.file_select_Click);
+            // 
+            // Convert_buttom
+            // 
+            this.Convert_buttom.Enabled = false;
+            this.Convert_buttom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Convert_buttom.Location = new System.Drawing.Point(119, 362);
+            this.Convert_buttom.Name = "Convert_buttom";
+            this.Convert_buttom.Size = new System.Drawing.Size(96, 34);
+            this.Convert_buttom.TabIndex = 4;
+            this.Convert_buttom.Text = "コンバート";
+            this.Convert_buttom.UseVisualStyleBackColor = true;
+            this.Convert_buttom.Click += new System.EventHandler(this.Comvert_buttom_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "|(.stl)|*.stl";
+            this.saveFileDialog.Filter = "\"(.stl)|*.stl|すべてのファイル|*.*\"";
+            // 
+            // gamma_bar
+            // 
+            this.gamma_bar.AutoSize = false;
+            this.gamma_bar.BackColor = System.Drawing.Color.White;
+            this.gamma_bar.Enabled = false;
+            this.gamma_bar.LargeChange = 1;
+            this.gamma_bar.Location = new System.Drawing.Point(15, 328);
+            this.gamma_bar.Maximum = 150;
+            this.gamma_bar.Name = "gamma_bar";
+            this.gamma_bar.Size = new System.Drawing.Size(200, 28);
+            this.gamma_bar.TabIndex = 20;
+            this.gamma_bar.TickFrequency = 10;
+            this.gamma_bar.Value = 9;
+            this.gamma_bar.Scroll += new System.EventHandler(this.gamma_bar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(232, 410);
+            this.Controls.Add(this.gamma_bar);
+            this.Controls.Add(this.Convert_buttom);
+            this.Controls.Add(this.file_select);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamma_bar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.Button file_select;
+        private System.Windows.Forms.Button Convert_buttom;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TrackBar gamma_bar;
+    }
+}
+
---
+++ source/AdphaneConverter/AdphaneConverter/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using System.IO;
+namespace Cvsharp_test
+{
+    public partial class Form1 : Form
+    {
+
+        private IplImage originalImg;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void file_select_Click(object sender, EventArgs e)
+        {
+            DialogResult dRet;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            if ((dRet = ofd.ShowDialog()) == DialogResult.OK)
+            {
+                //input_fname.Text = ofd.SafeFileName;
+                //input_fname.Text = ofd.FileName;
+                //pictureBoxIpl1.ImageIpl = new IplImage(input_fname.Text);
+                try
+                {
+                    originalImg = new IplImage(ofd.FileName);
+                    IplImage img =  Cv.CloneImage(originalImg);
+                    IplImage gray_img = Cv.CreateImage(Cv.Size(img.Width, img.Height), BitDepth.U8, 1);
+                    Cv.CvtColor(img, gray_img, ColorConversion.RgbToGray);
+                    Cv.CvtColor(gray_img, img, ColorConversion.GrayToRgb);
+                    gamma_bar.Value = 20;
+                    gamma_bar.Enabled = true;
+                    Convert_buttom.Enabled = true;
+                    update(img);
+                }
+                catch (OpenCvSharp.OpenCvSharpException ex)
+                {
+                    
+                    if(ex.Message == "Failed to create IplImage")
+                        MessageBox.Show("画像が開けません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    else
+                        MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                
+            }
+            else
+            {
+//                input_fname.Text = "";
+            }
+
+        }
+        unsafe private void Comvert_buttom_Click(object sender, EventArgs e)
+        {
+            DialogResult dRet;
+            IplImage source_image=pictureBoxIpl1.ImageIpl;
+            String output="";
+            UInt16 i, j;
+            byte* p = (byte*)source_image.ImageData;
+            if ((dRet = saveFileDialog.ShowDialog()) == DialogResult.OK)
+            {
+  //              input_fname.Text = saveFileDialog.FileName;
+                for (i = 0; i < source_image.Height; i++)
+                {
+                    for (j = 0; j < source_image.Width; j++)
+                    {
+                        output += string.Format("{0:f2}", 1.0 - (p[i * source_image.WidthStep + j * 3] / 255.0));
+                        if (j < source_image.Width - 1)
+                            output += " ";
+                        if (j == source_image.Width - 1)
+                            output += "\n";
+                    }
+                
+                }
+                
+                StreamWriter sw = new StreamWriter("output.dat", false, Encoding.ASCII);
+                sw.Write(output);
+                //end of writing dat file.
+                System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo();
+                psi.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;
+                psi.FileName = "openscad.com";
+                psi.Arguments = "-o " + saveFileDialog.FileName + " lith.scad";
+                System.Diagnostics.Process temp = System.Diagnostics.Process.Start(psi);
+                MessageBox.Show("STLファイル作成中です。しばらくお待ちください。");
+                temp.WaitForExit();
+                sw.Close();
+                MessageBox.Show("Complet!");
+            }
+            else
+            {
+    //            input_fname.Text = "";
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void gamma_bar_Scroll(object sender, EventArgs e)
+        {
+            changeParam();
+        }
+
+        private void changeParam()
+        {
+            CvMat source_img = ConvertFromIplImage(Cv.CloneImage(originalImg));
+            //CvMat source_img = ConvertFromIplImage(pictureBoxIpl1.ImageIpl);
+            CvMat gray_img = new CvMat(source_img.Height, source_img.Width, MatType.CV_8U);
+            Cv.CvtColor(source_img, gray_img, ColorConversion.RgbToGray);
+            double a = (gamma_bar.Value * 0.01 + 0.8);
+            //Cv.Threshold(gray_img, gray_img, 100,255, ThresholdType.Binary);
+            UInt32[] lut = new UInt32[256];
+            unsafe
+            {
+                byte* b = gray_img.DataByte;
+                for (int y = 0; y < gray_img.Height; y++)
+                {
+                    for (int x = 0; x < gray_img.Width; x++)
+                    {
+                        *b = (byte)(Math.Pow((double)*b / 255.0, (1.0 / a)) * 255);
+                        b++;
+                    }
+                }
+            }
+            //MessageBox.Show(a.ToString("F"));
+            /*for(int i = 0; i< 256; i++){
+                lut[i] = (UInt32)(Math.Pow((double)i / 255.0, 1.0 / a) * 255.0);
+                System.Diagnostics.Trace.WriteLine(lut[i]);
+            }
+            CvMat lut_mat = new CvMat(1, 256, MatrixType.U8C1, lut);
+            Cv.LUT(source_img, source_img, lut_mat);
+            */
+            //pictureBoxIpl1.RefreshIplImage(source_img);
+            //pictureBoxIpl1.ImageIpl = ConvertFromCvMat(source_img);
+            Cv.CvtColor(gray_img, source_img, ColorConversion.GrayToRgb);
+            update(ConvertFromCvMat(source_img));
+        }
+
+
+        private void update(IplImage image)
+        {
+            pictureBoxIpl1.ImageIpl = image;
+        }
+
+        private IplImage ConvertFromCvMat(CvMat mat){
+            return Cv.GetImage(mat);
+        }
+        private CvMat ConvertFromIplImage(IplImage image)
+        {
+            return Cv.GetMat(image);
+        }
+
+
+
+
+        
+
+    }
+}
